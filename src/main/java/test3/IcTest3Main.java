22abace279b902fe55fa0e5347545abb38202e18
Bernardino Frola <bfrola@groupon.com>
2017-04-14 19:15:11 +0100

    Test 1 and 3 setup

---
+++ src/main/java/test3/IcTest3Main.java
@@ -0,0 +1,17 @@
+package test3;
+
+public class IcTest3Main {
+    /**
+     * PROBLEM:
+     * We have some customer records in a text file (customers.json) -- one customer per line, JSON-encoded.
+     * We want to invite any customer within 100km of our Dublin office for some food and drinks on us.
+     * Write a program that will read the full list of customers and output the names and user ids of matching
+     * customers (within 100km), sorted by User ID (ascending).
+     *
+     * SOLUTION:
+     * 
+     */
+    public static void main(String args[]) {
+
+    }
+}
